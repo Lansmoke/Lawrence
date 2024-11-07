cf4fa0007e619c31f107d60b087058ac9f71bf21
Lansmoke <11344lawrencekigotho@gmail.com>
2024-11-07 18:29:28 +0300

    first commit

---
+++ CAT2_Q2B.c
@@ -0,0 +1,16 @@
+/*AUTHOR: Lawrence Kigotho Njoroge
+ADM NO: CT101/G/21996/24
+*/
+
+#include<stdio.h>
+int main() {
+	int p,q;
+	int scores[2][2]={{65,92},{85,72}};
+	
+	for(p=0;p<2;p++){
+		for(q=0;q<2;q++){
+			printf("scores[%d][%d] = %d \n", p,q,scores[p][q]);
+		}
+	}
+	return 0;
+}
